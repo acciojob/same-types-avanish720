59401aff836ca898c19ef15798c7e69418661cc6
Avanish Singh <107432071+avanishsinghav@users.noreply.github.com>
2025-01-26 12:46:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function isSameType(value1, value2) {
-  // Check if both values are NaN
-  if (Number.isNaN(value1) && Number.isNaN(value2)) {
+  //your js code here
+	if (Number.isNaN(value1) && Number.isNaN(value2)) {
     return true;
   }
 
